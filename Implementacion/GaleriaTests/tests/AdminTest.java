4fa313fcc8c2fca67ad9c639ce71aef84ffb0fef
FLACKIUS <felpg@192.168.0.105>
2024-05-12 17:45:09 -0500

    asdasd

--- Implementacion/GaleriaTests/tests/AdminTest.java
+++ Implementacion/GaleriaTests/tests/AdminTest.java
@@ -1,7 +1,20 @@
 package tests;
 
 
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class AdminTest {
 	
-	@
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
 }
